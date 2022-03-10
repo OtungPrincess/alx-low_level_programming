a71e949e285a38b19d34b9bcd0de50c92b3b0845
Otung Princess <otungprincess@gmail.com>
2022-03-10 13:06:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 1
+ * Return:1
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, ch, sizeof(ch));
-return (1);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
