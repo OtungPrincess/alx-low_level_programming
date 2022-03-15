44bcd4d9322afcf688e8eda2fbeaf2319e4b6e08
Otung Princess <otungprincess@gmail.com>
2022-03-15 08:22:26 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @pap: first number input
+ * @akara: second number input
+ * Return: pap + akara
+ */
+int add(int pap, int akara)
+{
+return (pap + akara);
+}
