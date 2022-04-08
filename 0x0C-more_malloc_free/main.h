ac5ca06e3f0e92084cb0e7182a761543dac4f829
Otung Princess <otungprincess@gmail.com>
2022-04-07 22:26:34 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int *int_calloc(int nmemb, unsigned int size);
-void mult(int *product, char *n1, char *n2, int len1, int len2);
-int is_valid(char *num);
-void err(int status);
-int main(int argc, char **argv);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
