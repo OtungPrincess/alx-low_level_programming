351a4a51a895169ee36ddb682d0833a9bcfa9fe9
Otung Princess <otungprincess@gmail.com>
2022-07-01 10:44:53 -0700

    hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int n;
-
+	
 	if (size == 0)
 		return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 		return (NULL);
-
-	for (n = 0; n < size; n++)
-	{
-		new_table->array[n] = NULL;
-	}
 	return (new_table);
 }
